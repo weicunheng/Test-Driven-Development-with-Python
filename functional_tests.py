8fb31cfa8bd0feb9f0552ddeea4aee7a5b62d726
Weicunheng <weicunheng@gmail.com>
2019-01-04 19:56:21 -0500

    使用unittest进行测试

--- functional_tests.py
+++ functional_tests.py
@@ -1,8 +1,24 @@
 from selenium import webdriver
+import unittest
 
 
-brower = webdriver.Firefox()
+class NewVisitTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-brower.get("http://172.16.1.61:8080")
+    def tearDown(self):
+        self.browser.quit()
 
-assert 'Django' in brower.title
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # 首先，需要访问TO-DO首页
+        self.browser.get("http://172.16.1.61:8080")
+
+        # 查看网站标题中是否包含"TO-DO"
+        self.assertIn("TO-DO",self.browser.title)
+
+        self.fail(msg="完成测试")  # 提醒测试执行结束
+
+
+if __name__ == '__main__':
+    # 程序会在文件中，自动查找测试类和方法，然后运行
+    unittest.main(warnings="ignore")
