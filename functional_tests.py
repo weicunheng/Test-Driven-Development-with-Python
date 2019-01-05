8ad34c7b3f1287bdca1db0fcec2f02864a396e96
Weicunheng <weicunheng@gmail.com>
2019-01-04 20:00:05 -0500

    等待Selenium加载完成页面，在执行操作

--- functional_tests.py
+++ functional_tests.py
@@ -5,6 +5,7 @@ import unittest
 class NewVisitTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
